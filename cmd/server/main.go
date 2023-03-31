98f559a16a654b5059cfb050ed090d8e528224ac
Made Indra <made.indra@protonmail.com>
2023-03-31 15:30:46 +0800

    add processing after reading each line

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"path/filepath"
 	"runtime"
+	"sync"
 
 	"github.com/madeindra/golang-stream-processing/internal/model"
 	"github.com/madeindra/golang-stream-processing/pkg/reader"
@@ -17,27 +18,42 @@ func main() {
 		panic("failed reading directory")
 	}
 
+	// map as temporary database
+	stockData := make(map[string]model.StockDetail)
+
+	// sync for goroutines
+	var wg sync.WaitGroup
+	var mtx sync.Mutex
+
 	// read all files and pass the result to the function
-	if err := reader.ReadAllFiles(filepath.Join(dir, "../../../data"), passLine); err != nil {
+	if err := reader.ReadAllFiles(filepath.Join(dir, "../../../data"), passLine(stockData, &wg, &mtx)); err != nil {
 		fmt.Println("error: ", err)
 	}
+
+	wg.Wait()
+	fmt.Println(stockData)
 }
 
-func passLine(lines string) error {
+func passLine(stockData map[string]model.StockDetail, wg *sync.WaitGroup, mtx *sync.Mutex) func(lines string) error {
 	// create error channel
 	chErr := make(chan error)
-	defer close(chErr)
 
-	// pass to goroutines
-	go process(lines, chErr)
-	if err := <-chErr; err != nil {
-		return err
+	return func(lines string) error {
+		wg.Add(1)
+
+		// pass to goroutines
+		go process(lines, stockData, chErr, wg, mtx)
+		if err := <-chErr; err != nil {
+			return err
+		}
+
+		return nil
 	}
-
-	return nil
 }
 
-func process(lines string, chErr chan<- error) {
+func process(lines string, stockData map[string]model.StockDetail, chErr chan<- error, wg *sync.WaitGroup, mtx *sync.Mutex) {
+	defer wg.Done()
+
 	// parse json
 	data := &model.Data{}
 	if err := json.Unmarshal([]byte(lines), data); err != nil {
@@ -45,7 +61,20 @@ func process(lines string, chErr chan<- error) {
 		return
 	}
 
-	// print parsed line of data
-	fmt.Println(data)
+	// retrive value
+	stockDetail := stockData[data.Name]
+
+	if stockDetail.Name == "" {
+		stockDetail.Name = data.Name
+	}
+
+	stockDetail.Quantity += data.Quantity
+	stockDetail.AveragePrice = float64(stockDetail.Quantity) / float64(data.Price)
+
+	// modify map
+	mtx.Lock()
+	stockData[data.Name] = stockDetail
+	mtx.Unlock()
+
 	chErr <- nil
 }
